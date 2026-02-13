cc5f491bfc934334b47a554a3c2521c3463cc02e
Allan Paz <jallanpazg@gmail.com>
2026-02-12 20:31:23 -0700

    updated app and car component

--- 04_reactjs/src/App.jsx
+++ 04_reactjs/src/App.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import Car from "./components/Car";
 
 const App = () => {
-  return <div>App</div>;
+  const [cars, setCars] = useState([]);
+
+  useEffect(() => {
+    fetch("api/v1/cars")
+      .then((res) => res.json())
+      .then((data) => setCars(data))
+      .catch((err) => console.log(err));
+  }, []);
+
+  return (
+    <div>
+      <h1>Welcome to the Car Store</h1>
+
+      <ul>
+        {cars.map((car) => (
+          <Car key={car.id} {...car} />
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default App;
